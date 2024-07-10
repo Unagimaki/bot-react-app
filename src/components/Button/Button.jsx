2480882cf914f52acd138a9d2249d01e6ce220b0
Mikhail <mikhailyarkov2000@gmail.com>
2024-07-10 12:29:21 +0400

    added button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import './button.css'
 export const Button = ({handleClick}) => {
     return(
         <>
-            <a onClick={() => handleClick()} href="#" class="btn-flip" data-back="Translated" data-front="Translate"></a>
+            <a onClick={() => handleClick()} class="btn-flip" data-back="Translated" data-front="Translate"></a>
         </>
     )
 }
